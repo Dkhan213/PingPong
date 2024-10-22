1a09e4816e30f702093fe9864c1eed1b53a8d317
User2 <dkhan321@gmail.com>
2024-10-22 16:29:15 -0500

    Added paddles.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
+
+class Paddle
+{
+private:
+    sf::RectangleShape paddle;
+public:
+    Paddle(float x, float y)
+    {
+        paddle.setSize(sf::Vector2f(10, 100));
+        paddle.setPosition(x, y);
+        paddle.setFillColor(sf::Color::White);
+    }
+
+    sf::RectangleShape getShape() const 
+    {
+        return paddle;
+    };
+};
 
 const int windowWidth = 800;
 const int windowHeight = 600;
@@ -8,19 +24,23 @@ const int windowHeight = 600;
 int main() {
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Window");
 
-    while (window.isOpen()) {
+    Paddle leftPaddle(10, windowHeight / 2 - 50);
+    Paddle rightPaddle(windowWidth - 20, windowHeight / 2 - 50);
+
+    // Game loop
+    while (window.isOpen())
+    {
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event))
+        {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         // Clear and draw
         window.clear();
+        window.draw(leftPaddle.getShape());
+        window.draw(rightPaddle.getShape());
         window.display();
     }
-
-
-
     return 0;
 }
